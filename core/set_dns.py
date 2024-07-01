bf08a28bcc0b4aa8637bca3d7855a5ba0462b10c
parsa 002 <parsa002am@gmail.com>
2024-07-01 10:58:11 +0330

    Update set_dns.py

--- core/set_dns.py
+++ core/set_dns.py
@@ -22,6 +22,24 @@ def get_linux_interface():
         return interface
     except:
         return None
+    
+def install_network_manager():
+    try:
+        subprocess.run(['sudo', 'apt-get', 'update'], check=True)
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'network-manager'], check=True)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"خطا در نصب NetworkManager: {e}")
+        return False
+
+def get_connection_name():
+    try:
+        result = subprocess.run(['nmcli', '-t', '-f', 'NAME', 'connection', 'show'], capture_output=True, text=True)
+        connections = result.stdout.strip().split('\n')
+        # برگرداندن اولین اتصال موجود
+        return connections[0] if connections else None
+    except:
+        return None
 
 def set_dns_option(option):
     if not is_admin():
@@ -44,17 +62,25 @@ def set_dns_option(option):
             subprocess.run(['powershell', '-Command', f"Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses '{primary_dns}','{secondary_dns}'"])
             return {'success': True, 'msg': f"Primary DNS set to {primary_dns} and Secondary DNS set to {secondary_dns} for Windows."}
     elif system == 'Linux':
-        interface = get_linux_interface()
+        try:
+            subprocess.run(['nmcli', '-v'], check=True)
+        except FileNotFoundError:
+            if not install_network_manager():
+                return {'success': False, 'code': '500', 'msg': "NetworkManager  - not installed."}
+        connection_name  = get_connection_name()
+        if not connection_name:
+            return {'success': False, 'code': '500', 'msg': "Network connection not found."}
+        
         if option == 0:
-            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.ignore-auto-dns', 'no'])
-            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.dns', ''])
-            subprocess.run(['nmcli', 'con', 'up', interface])
+            subprocess.run(['nmcli', 'con', 'mod', connection_name , 'ipv4.ignore-auto-dns', 'no'])
+            subprocess.run(['nmcli', 'con', 'mod', connection_name , 'ipv4.dns', ''])
+            subprocess.run(['nmcli', 'con', 'up', connection_name ])
             return {'success': True, 'msg': "DNS servers reset for Linux."}
         else:
             dns_addresses = f"{primary_dns},{secondary_dns}"
-            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.dns', dns_addresses])
-            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.ignore-auto-dns', 'yes'])
-            subprocess.run(['nmcli', 'con', 'up', interface])
+            subprocess.run(['nmcli', 'con', 'mod', connection_name , 'ipv4.dns', dns_addresses])
+            subprocess.run(['nmcli', 'con', 'mod', connection_name , 'ipv4.ignore-auto-dns', 'yes'])
+            subprocess.run(['nmcli', 'con', 'up', connection_name ])
             return {'success': True, 'msg': f"Primary DNS set to {primary_dns} and Secondary DNS set to {secondary_dns} for Linux."}
     else:
         return {'success': False, 'code': '500', 'msg': "Unsupported operating system."}
