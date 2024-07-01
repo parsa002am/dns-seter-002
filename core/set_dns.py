739809063760290758a08861206ea19422c2d5de
parsa 002 <parsa002am@gmail.com>
2024-07-01 10:29:07 +0330

    add linux system to set_dns
    support linux system

--- core/set_dns.py
+++ core/set_dns.py
@@ -1,37 +1,60 @@
 import subprocess
 import platform
 import ctypes
+import os
 
 from core.Query import DNSQuery
 
 
 def is_admin():
-            try:
-                return ctypes.windll.shell32.IsUserAnAdmin()
-            except:
-                return False
+    try:
+        if platform.system() == "Windows":
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        else:
+            return os.geteuid() == 0
+    except:
+        return False
+
+def get_linux_interface():
+    try:
+        result = subprocess.run(['ip', '-o', '-4', 'route', 'show', 'to', 'default'], capture_output=True, text=True)
+        interface = result.stdout.split()[4]
+        return interface
+    except:
+        return None
 
 def set_dns_option(option):
     if not is_admin():
-        return {'success':False , 'code' : '500' , 'msg':"The application is NOT running with administrative privileges."}
+        return {'success': False, 'code': '500', 'msg': "The application is NOT running with administrative privileges."}
 
-    if(option != 0):
+    if option != 0:
         try:
             dns = DNSQuery().get_id(option)
-            primary_dns= dns.primary_dns
+            primary_dns = dns.primary_dns
             secondary_dns = dns.secondary_dns
         except:
-            return {'success':False , 'code' : '400' , 'msg':'Invalid DNS selected'}
-    
+            return {'success': False, 'code': '400', 'msg': 'Invalid DNS selected'}
+
     system = platform.system()
-    if system == 'Windows':   
+    if system == 'Windows':
         if option == 0:
             subprocess.run(['powershell', '-Command', 'Set-DnsClientServerAddress "*" -ResetServerAddresses'])
-            return {'success':True , 'msg':"DNS servers reset for Windows."}
+            return {'success': True, 'msg': "DNS servers reset for Windows."}
         else:
             subprocess.run(['powershell', '-Command', f"Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses '{primary_dns}','{secondary_dns}'"])
-            return {'success':True , 'msg':f"Primary DNS set to {primary_dns} and Secondary DNS set to {secondary_dns} for Windows."}
-            
-    
+            return {'success': True, 'msg': f"Primary DNS set to {primary_dns} and Secondary DNS set to {secondary_dns} for Windows."}
+    elif system == 'Linux':
+        interface = get_linux_interface()
+        if option == 0:
+            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.ignore-auto-dns', 'no'])
+            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.dns', ''])
+            subprocess.run(['nmcli', 'con', 'up', interface])
+            return {'success': True, 'msg': "DNS servers reset for Linux."}
+        else:
+            dns_addresses = f"{primary_dns},{secondary_dns}"
+            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.dns', dns_addresses])
+            subprocess.run(['nmcli', 'con', 'mod', interface, 'ipv4.ignore-auto-dns', 'yes'])
+            subprocess.run(['nmcli', 'con', 'up', interface])
+            return {'success': True, 'msg': f"Primary DNS set to {primary_dns} and Secondary DNS set to {secondary_dns} for Linux."}
     else:
-        return {'success':False , 'code' : '500' , 'msg':"Unsupported operating system."}
+        return {'success': False, 'code': '500', 'msg': "Unsupported operating system."}
